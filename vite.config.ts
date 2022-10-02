0d8d38baa5df7ac8673ea45751673a10aa0d54dd
AkaraChen <85140972+AkaraChen@users.noreply.github.com>
2022-10-02 16:18:56 +0800

    ✨ feat: pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import {defineConfig} from 'vite';
 import preact from '@preact/preset-vite';
+import {VitePWA} from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [preact(), VitePWA()],
 });
